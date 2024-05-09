812e0da3ba29a5c6c694cb500ab9bf2bf26ad47e
Capitan Cloud <my25mb@has.im>
2024-05-09 16:05:22 +0200

    Add files via upload

--- index.php
+++ index.php
@@ -10,5 +10,18 @@ $gd_image_editor->load();
 //$gd_image_editor->resize(300,450,false); 
 $gd_image_editor->rotate(90);
 $gd_image_editor->save($img_path);
-echo("<br>&nbsp;&nbsp;Image rotated!");
+//echo("<br>&nbsp;&nbsp;Image rotated!");
 ?>
+
+<pre>
+$img_path = '~/img/file.jpg';
+$gd_image_editor = new WP_Image_Editor_GD($img_path); 
+$gd_image_editor->load(); 
+$gd_image_editor->rotate(90);
+$gd_image_editor->save($img_path);
+</pre>
+
+<br><br>
+<br>&nbsp;&nbsp;Image rotated!   
+<br><br><br>
+&nbsp;&nbsp;<img src='file.jpg' style="height:350px;"> 
